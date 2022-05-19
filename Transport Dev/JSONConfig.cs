8ca8bbb716ab7d872b0ef74ce223eda91d248a5e
wesseb <wesseb95@gmail.com>
2022-05-19 13:17:04 +0200

    .

--- Transport Dev/JSONConfig.cs
+++ Transport Dev/JSONConfig.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Transport_Dev
 {
-    public class SQL
+
+    internal class JSONConfig
     {
 
     }
